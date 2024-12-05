67d3b1ed19829559f1203f5e2d63c6b1f86274b0
h1k0 <h1k0naka@outlook.com>
2024-12-05 15:38:53 +0800

    update rectangles/main.rs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -31,5 +31,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("rect1 area: {}", rect1.area());
+    println!("rect2 area: {}", rect2.area());
 }
 
